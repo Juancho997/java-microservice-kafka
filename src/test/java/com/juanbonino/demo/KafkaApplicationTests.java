928869dc177dac0d2931459ff516f6eed6a4546a
Juan Bonino <tomasboni9@gmail.com>
2022-10-19 21:24:42 -0300

    init: kafka project

---
+++ src/test/java/com/juanbonino/demo/KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanbonino.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
